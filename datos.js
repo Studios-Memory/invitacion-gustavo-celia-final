8bb5c09554ee4fde8713dc8beb32f4563335d7af
Studios-Memory <studiosmemory1@gmail.com>
2026-08-05 17:00:19 -0400

    Add files via upload

---
+++ datos.js
@@ -0,0 +1,44 @@
+window.DATOS_INVITACION = {
+  // Tipo: "boda" o "graduacion"
+  tipo: "boda",
+
+  // Información principal
+  titulo: "Nuestra Boda",
+  nombres: "Gustavo y Celia",
+  iniciales: "G & C",
+  fecha: "22 de agosto de 2026",
+  mensajePrincipal:
+    "Nos complace invitarte a ser parte de nuestro gran día.",
+
+  // Ceremonia
+  ceremonia: {
+    titulo: "Ceremonia",
+    lugar: "Nombre de la iglesia",
+    hora: "15:00",
+    ubicacion: "PEGA_AQUI_EL_ENLACE_DE_GOOGLE_MAPS"
+  },
+
+  // Recepción
+  recepcion: {
+    titulo: "Recepción",
+    lugar: "Nombre del salón",
+    hora: "18:00",
+    ubicacion: "PEGA_AQUI_EL_ENLACE_DE_GOOGLE_MAPS"
+  },
+
+  // Imágenes
+  imagenes: {
+    portada: "img/portada1.jpg",
+    vistaPrevia: "img/preview.jpg"
+  },
+
+  // Música
+  musica: "music/poeta.mp3",
+
+  // Información para compartir
+  whatsapp: {
+    titulo: "Nuestra Boda | Gustavo y Celia",
+    descripcion:
+      "Nos complace invitarte a ser parte de nuestro gran día."
+  }
+};
